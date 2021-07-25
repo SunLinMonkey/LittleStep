82ac2123111395f96722e8b3a310bb2784910908
Mr.Sun <17779258+SUNLINKAKA@users.noreply.github.com>
2021-07-25 19:53:02 +0800

    添加数据库

---
+++ database/src/main/java/com/example/database/entity/ConfigGlobal.java
@@ -0,0 +1,86 @@
+package com.example.database.entity;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+
+import java.util.Date;
+
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * create on 2021/7/25 17:20
+ * Email:923998007@qq.com
+ *
+ * @author lin
+ */
+@Entity
+public class ConfigGlobal {
+    @Id
+    private int id;
+    private String configKey;
+    private String configValue;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    @Generated(hash = 63524207)
+    public ConfigGlobal(int id, String configKey, String configValue,
+                        Date createTime, Date updateTime) {
+        this.id = id;
+        this.configKey = configKey;
+        this.configValue = configValue;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    @Generated(hash = 415791402)
+    public ConfigGlobal() {
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getConfigKey() {
+        return this.configKey;
+    }
+
+    public void setConfigKey(String configKey) {
+        this.configKey = configKey;
+    }
+
+    public String getConfigValue() {
+        return this.configValue;
+    }
+
+    public void setConfigValue(String configValue) {
+        this.configValue = configValue;
+    }
+
+    public Date getCreateTime() {
+        return this.createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return this.updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
